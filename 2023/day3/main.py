a8015524696e0ce40dff1611efaeedbdc64e3b3f
flieh <ruedelaflieh@gmail.com>
2023-12-26 18:03:54 +0100

    .

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -12,36 +12,43 @@ def fetch():
 
 
 def part_one():
+    breakpoint()
     data = fetch()
     numbers = []
-    symbols = []
     retVal = 0
-    for lineNo, line in enumerate(data):
+    tempData = []
+    for line in data:
+        tempLine = line
+        for char in line:
+            if not char.isdigit():
+                tempLine = tempLine.replace(char, ".")
+        tempData.append(tempLine)
+    for lineNo, line in enumerate(tempData):
         for token in line.split("."):
-            wtoken = token
-            for char in token:
-                if not char.isdigit() and len(wtoken) > 1:
-                    wtoken = token.replace(char, "")
-                    symbols.append((char, (lineNo, line.find(wtoken))))
-            if len(wtoken) == 1 and not char.isdigit():
-                symbols.append((wtoken, (lineNo, line.find(wtoken))))
-            elif len(wtoken) > 0:
-                numbers.append((wtoken, (lineNo, line.find(wtoken))))
+            if len(token) > 0:
+                numbers.append((token, (lineNo, line.find(token))))
+    tempData = data
+    symbols = []
+    for lineNo, line in enumerate(tempData):
+        for token in line:
+            if (token.isdigit() == False) and (len(token) == 1) and (token != "."):
+                symbols.append((token, (lineNo, line.find(token))))
+    # print(symbols)
     for number in numbers:
         for symbol in symbols:
-            # pdb.set_trace()
             if (
                 int(number[1][0]) - 2 < int(symbol[1][0])
                 and int(symbol[1][0]) < int(number[1][0]) + 2
                 and int(number[1][1]) - 2 < int(symbol[1][1])
                 and int(symbol[1][1]) < int(number[1][1]) + len(number) + 2
             ):
-                print(number[0])
+                print(int(number[0]))
                 retVal += int(number[0])
                 break
 
     print(retVal)
     # print(symbols)
+    # print(numbers)
 
 
 def part_two():
