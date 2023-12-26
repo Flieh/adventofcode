a12fd6105b02f2bc1c327e7cb5745c8f7ec01191
flieh <ruedelaflieh@gmail.com>
2023-12-26 12:06:49 +0100

    a

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -1,4 +1,5 @@
 """ aoc 2023 dayx """
+import pdb
 
 
 def fetch():
@@ -13,24 +14,34 @@ def fetch():
 def part_one():
     data = fetch()
     numbers = []
-    for line in data:
+    symbols = []
+    retVal = 0
+    for lineNo, line in enumerate(data):
         for token in line.split("."):
             wtoken = token
             for char in token:
-                if not char.isdigit():
+                if not char.isdigit() and len(wtoken) > 1:
                     wtoken = token.replace(char, "")
-            if len(wtoken) > 0:
-                numbers.append(wtoken)
+                    symbols.append((char, (lineNo, line.find(wtoken))))
+            if len(wtoken) == 1 and not char.isdigit():
+                symbols.append((wtoken, (lineNo, line.find(wtoken))))
+            elif len(wtoken) > 0:
+                numbers.append((wtoken, (lineNo, line.find(wtoken))))
     for number in numbers:
-        field = []
-        for lineNo, line in enumerate(data):
-            if line.find(number) > -1:
-                if lineNo > 0:
-                    if line.find(number) > 0:
-                        field.append(lineNo - 1, line.find(number) - 1)
-                    field.append(lineNo -1,)
+        for symbol in symbols:
+            # pdb.set_trace()
+            if (
+                int(number[1][0]) - 2 < int(symbol[1][0])
+                and int(symbol[1][0]) < int(number[1][0]) + 2
+                and int(number[1][1]) - 2 < int(symbol[1][1])
+                and int(symbol[1][1]) < int(number[1][1]) + len(number) + 2
+            ):
+                print(number[0])
+                retVal += int(number[0])
+                break
 
-    print(coordSymbols)
+    print(retVal)
+    # print(symbols)
 
 
 def part_two():
