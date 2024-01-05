703544a9128b0fac2375f291bb07f7ff3675ef54
Flieh <ruedelaflieh@gmail.com>
2024-01-05 08:29:58 +0100

    .

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -1,5 +1,6 @@
 """ aoc 2023 dayx """
 import pdb
+import re
 
 
 def fetch():
@@ -17,35 +18,45 @@ def part_one():
     retVal = 0
     tempData = []
     for line in data:
-        tempLine = line
-        for char in line:
+        tempLine = ''
+        for charIndex, char in enumerate(list(line)):
             if not char.isdigit():
-                tempLine = tempLine.replace(char, ".")
+                tempLine += ' '
+            else:
+                tempLine += char
         tempData.append(tempLine)
+    # print(tempData)
     for lineNo, line in enumerate(tempData):
-        for token in line.split("."):
+        for token in line.split(' '):
             if len(token) > 0:
-                numbers.append((token, (lineNo, line.find(token))))
+                pat = '\b' + token + '\b'
+                print(pat)
+                print(line)
+                print(re.search(line, pat))
+                # numbers.append((token, (lineNo, re.search(pat, line).span()[0])))
+        # print(numbers)
     tempData = data
     symbols = []
     for lineNo, line in enumerate(tempData):
-        for token in line:
-            if (token.isdigit() == False) and (len(token) == 1) and (token != "."):
-                symbols.append((token, (lineNo, line.find(token))))
+        for charInd, char in enumerate(line):
+            if (not char.isdigit()) and char != ".":
+                symbols.append((char, (lineNo, charInd)))
     # print(symbols)
+    validParts = []
     for number in numbers:
         for symbol in symbols:
-            breakpoint()
-            if (
-                int(number[1][0]) - 2 < int(symbol[1][0])
-                and int(symbol[1][0]) < int(number[1][0]) + 2
-                and int(number[1][1]) - 2 < int(symbol[1][1])
-                and int(symbol[1][1]) < int(number[1][1]) + len(number) + 2
-            ):
-                print(int(number[0]))
-                retVal += int(number[0])
+            # breakpoint()
+            if (  int(number[1][0]) - 2 < int(symbol[1][0])
+              and int(symbol[1][0])     < int(number[1][0]) + 2
+              and int(number[1][1]) - 2 < int(symbol[1][1])
+              and int(symbol[1][1])     < int(number[1][1]) + len(number[0]) + 1):
+                if int(number[0]) == 4:
+                    print(symbol)
+                    print(number)
+                validParts.append(int(number[0]))
                 break
-
+    retVal = sum(validParts)
+    print(sorted(validParts))
     print(retVal)
     # print(symbols)
     # print(numbers)
